9e5f93903cdeb90e539a785a30e3b05b3663c2e4
ndakalajulius <julius.ndakala@student.moringaschool>
2022-08-30 13:45:04 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,19 +1,25 @@
 // Write your code here
 class Breakfast {
     constructor( food , drink) {
-      
+      this.food = food;
+      this.drink = drink;
     }
   }
-  class Blunch {
+  class lunch {
     constructor( salad, soup , drink) {
       
-      
+      this.salad = salad;
+      this.soup = soup;
+      this.drink= drink;
     }
   }
 
   class dinner {
 #dessert;
     constructor( salad, soup, entree) {
-     
+    this.#dessert = dessert;
+    this.salad = salad;
+      this.soup = soup;
+      this.entree = entree;
   }
   }
